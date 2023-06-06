2472d5efe8edc8a04a081b0b31dd4fd0db44fbd0
Oscar Aguilar <aguilop.oscar@gmail.com>
2023-06-06 10:18:24 -0600

    fix: change redirection route after successful order

--- lib/pages/success_page.dart
+++ lib/pages/success_page.dart
@@ -31,18 +31,18 @@ class SuccessPage extends StatelessWidget {
               style: TextStyle(fontSize: 32),
             ),
             const SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () async {
-            //     var url = Uri.parse('https://www.example.com');
-            //     if (!await launchUrl(url)) {
-            //       throw Exception('Could not launch $url');
-            //     }
-            //   },
-            //   child: const Text("Realizar encuesta de satisfacción"),
-            // ),
+            ElevatedButton(
+              onPressed: () async {
+                var url = Uri.parse(
+                  'https://docs.google.com/forms/d/e/1FAIpQLSe3EdG-p6Zra3fE3BUA2zinR0Weu_WbM-f-ebxGi4ps-aw71w/viewform?usp=sharing',
+                );
+                _launchUrl(url);
+              },
+              child: const Text("Realizar encuesta de satisfacción"),
+            ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => Navigator.pushReplacementNamed(context, "/"),
+              onPressed: () => Navigator.pushReplacementNamed(context, "/home"),
               child: const Text("Volver a inicio"),
             ),
           ],
@@ -52,8 +52,10 @@ class SuccessPage extends StatelessWidget {
   }
 
   Future<void> _launchUrl(Uri url) async {
-    if (!await launchUrl(url)) {
+    if (await canLaunchUrl(url)) {
       throw Exception('Could not launch $url');
     }
+
+    await launchUrl(url, mode: LaunchMode.inAppWebView);
   }
 }
